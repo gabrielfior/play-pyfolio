467530f54a02963851cb2fcbb4b3f60e85aa02e9
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2020-12-06 19:10:08 +0100

    deleted data files | reorganized repo files in notebooks

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='buy_hold_analyser',
+version='0.1',
+description='Buy-and hold strategies analyser',
+url='#',
+author='gabrielfior',
+author_email='gabriel.fior@tum.de',
+license='MIT',
+packages=['buy_hold_analyser'],
+zip_safe=False)
